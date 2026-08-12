5651423b68c5fc9a698c9519c4de7436496994c3
annycastro035-lab <annycastro035@gmail.com>
2026-08-12 17:16:04 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,14 +1,42 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORTA = process.env.PORT || 3000;
 
 const servidor = http.createServer((req, res) => {
-res.writeHead(200, { 'Content-Type': 'application/json' });
+  let arquivo = req.url === '/' ? 'index.html' : req.url.substring(1);
 
-res.end(JSON.stringify({
-mensagem: 'Servidor do QuizUp funcionando!',
-status: 'online'
-}));
+  const caminho = path.join(__dirname, arquivo);
+
+  fs.readFile(caminho, (erro, conteudo) => {
+    if (erro) {
+      res.writeHead(404, {
+        'Content-Type': 'text/plain; charset=utf-8'
+      });
+
+      res.end('Arquivo não encontrado.');
+      return;
+    }
+
+    let tipo = 'text/html';
+
+    if (arquivo.endsWith('.css')) {
+      tipo = 'text/css';
+    }
+
+    if (arquivo.endsWith('.js')) {
+      tipo = 'application/javascript';
+    }
+
+    res.writeHead(200, {
+      'Content-Type': tipo
+    });
+
+    res.end(conteudo);
+  });
 });
 
-servidor.listen(3000, () => {
-console.log('Servidor rodando na porta 3000');
+servidor.listen(PORTA, () => {
+  console.log(`QuizUp funcionando na porta ${PORTA}`);
 });
